b07aaed876a145eb7b6e73db71ec43645bb8bc2d
debayudh2001 <152640749+debayudh2001@users.noreply.github.com>
2025-02-14 03:11:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let next = document.querySelector("#next")
+let prev = document.querySelector("#prev")
+let circles = document.querySelectorAll(".circle")
+//console.log(circles)
+let ind = 1
+next.addEventListener("click", () => {
+	circles[ind].classList.add("active")
+	ind += 1
+	prev.disabled = false
+	if(ind === circles.length){
+		next.disabled = true
+	}
+})
+prev.addEventListener("click", () => {
+	ind -= 1
+	circles[ind].classList.remove("active")
+	next.disabled = false
+	if(ind === 1){
+		prev.disabled = true
+	}
+})
